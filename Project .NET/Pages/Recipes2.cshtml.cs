32d61d34ae1ac598bc2e59f16d9f24221dfbf1dc
Ridek240 <64654063+Ridek240@users.noreply.github.com>
2021-05-20 16:45:41 +0200

    zatwierdz

--- Project .NET/Pages/Recipes2.cshtml.cs
+++ Project .NET/Pages/Recipes2.cshtml.cs
@@ -2,12 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Project_.NET.Data;
 using Project_.NET.Models;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Authorization;
 namespace Project_.NET.Pages.Shared
 {
     public class Recipes2Model : PageModel
@@ -26,7 +25,51 @@ namespace Project_.NET.Pages.Shared
             RP = RPQuerry.ToList();
         }
 
-
+        public async Task<ActionResult> OnPostDeleteAsync(int itemId, string userId)
+        {
+            if (userId != null && userId.CompareTo(GetUserId()) == 0)
+            DelRep(itemId);
+            return  RedirectToPage("./Recipes2");
+        }
+        public async Task<ActionResult> OnPostEditAsync()
+        {
+            return  RedirectToPage("./Recipes");
+        }
+        public async Task<ActionResult> OnPostLikeAsync(int itemId, string userId)
+        {
+            Recipes RPUp = (from Recipes in _cont.Recipes where Recipes.Id == itemId orderby Recipes.date select Recipes).FirstOrDefault();
+            if (RPUp != null)
+            {
+                RPUp.up_vote = RPUp.up_vote + 1;
+                _cont.Recipes.Update(RPUp);
+                _cont.SaveChanges();
+            }
+            return RedirectToPage("./Recipes2");
+        }
+        public async Task<ActionResult> OnPostHateAsync(int itemId, string userId)
+        {
+            Recipes RPUp = (from Recipes in _cont.Recipes where Recipes.Id == itemId orderby Recipes.date select Recipes).FirstOrDefault();
+            if (RPUp != null)
+            {
+                RPUp.up_vote = RPUp.up_vote - 1;
+                _cont.Recipes.Update(RPUp);
+                _cont.SaveChanges();
+            }
+            return RedirectToPage("./Recipes2");
+        }
+        public async Task<ActionResult> OnPostFavoriteAsync()
+        {
+            return RedirectToPage("./Index");
+        }
+        public void DelRep(int i)
+        {
+            Recipes RPDel = (from Recipes in _cont.Recipes where Recipes.Id == i orderby Recipes.date select Recipes).FirstOrDefault();
+            if(RPDel != null)
+            {
+                _cont.Recipes.Remove(RPDel);
+                _cont.SaveChanges();
+            }
+        }
         public bool UserProperty(string iduser)
         {
             if (iduser == _userManager.GetUserId(HttpContext.User))
@@ -34,5 +77,17 @@ namespace Project_.NET.Pages.Shared
             else
                 return false;
         }
+        public string GetUserName(string userId)
+        {
+            var username = _userManager.FindByIdAsync(userId).Result;
+            if (username != null)
+                return username.UserName;
+            return "Anonim";
+        }
+        public string GetUserId()
+        {
+            return _userManager.GetUserId(HttpContext.User);
+        }
+       
     }
 }
